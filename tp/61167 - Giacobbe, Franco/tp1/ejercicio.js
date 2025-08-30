1a4311e296385df4b625d143f3bff976e8c1a8fa
Franco Giacobbe <frandg47@gmail.com>
2025-08-30 00:46:32 -0300

    creando funcionalidad de cargar, guardar y registrar datos en el archivo agenda.json

--- tp/61167 - Giacobbe, Franco/tp1/ejercicio.js
+++ tp/61167 - Giacobbe, Franco/tp1/ejercicio.js
@@ -1,24 +1,43 @@
-import {prompt, read, write} from './io.js';
+import { prompt, read, write } from "./io.js";
 
 class Contacto {
-    /*...*/
+  constructor(nombre, apellido, edad, telefono, email) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.telefono = telefono;
+    this.email = email;
+  }
 }
 
 class Agenda {
-    agregar(contacto){/*...*/}
-    static async cargar(){ return new Agenda();/*...*/ }
-    async guardar(){/*...*/}
+  constructor() {
+    this.contactos = [];
+  }
+  agregar(contacto) {
+    Agenda.cargar();
+    this.contactos.push(contacto);
+  }
+  static async cargar() {
+    const data = await read("./agenda.json");
+    const agenda = new Agenda();
+    agenda.contactos = JSON.parse(data);
+    return agenda;
+  }
+  async guardar() {
+    const data = this.contactos;
+    await write(JSON.stringify(data, null, 2));
+  }
 }
-
 
 // EJEMPLO DE USO... borrar...
 let agenda = await Agenda.cargar();
-
+console.log(agenda);
 console.log("=== Ingresar nuevo contacto ===");
 
 let c = new Contacto();
 c.nombre = await prompt("Nombre :>");
-c.edad   = await prompt("Edad   :>");
+c.edad = await prompt("Edad   :>");
 agenda.agregar(c);
 
-await agenda.guardar(); 
+await agenda.guardar();
