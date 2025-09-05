ecadafef02a7d1d341a562256e3bb30fe803c44e
Tito <thiagorobles3@gmail.com>
2025-09-05 16:31:14 -0300

    Estructura basica del proyecto

--- tp/61315 - Robles,Thiago/tp2/ejercicio.js
+++ tp/61315 - Robles,Thiago/tp2/ejercicio.js
@@ -1,2 +1,39 @@
 'use strict';
 // Funciones generales
+
+
+
+class Agenda {
+
+    static idAutoincrementable = 1;
+    
+
+    constructor(){
+        this.contactos = [];
+    }
+
+
+}
+
+class Contacto {
+
+    constructor(){
+        this.id = id;
+        this. nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+
+    agregarContacto(contacto){
+
+        contacto.id = Agenda.
+
+
+    }
+
+
+
+
+}
