562bbd04dd9807eefd66e46dec5620389bef47aa
Tito <thiagorobles3@gmail.com>
2025-09-05 19:20:34 -0300

    Agregando funcionabilidad de mostrar el modal con el boton Agregar

--- tp/61315 - Robles,Thiago/tp2/ejercicio.js
+++ tp/61315 - Robles,Thiago/tp2/ejercicio.js
@@ -1,37 +1,47 @@
 'use strict';
+
+
+// DOM
+
+
+//Modales
+const modalAgregar = document.getElementById('modal-agregar');
+
+
+//Botones
+const btnAgregar = document.getElementById('btn-agregar');
+
 // Funciones generales
+
+// Al hacer click en "Agregar contacto", se abre el modal
+btnAgregar.addEventListener('click', () => {    
+    modalAgregar.showModal();
+});
+
+
+
+
 
 
 
 class Agenda {
 
     static idAutoincrementable = 1;
-    
-
     constructor(){
         this.contactos = [];
     }
 
-
 }
 
 class Contacto {
-
-    constructor(){
-        this.id = id;
-        this. nombre = nombre;
+    constructor(nombre, apellido, telefono, email){
+        this.id = Agenda.idAutoincrementable++;
+        this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
         this.email = email;
     }
 
-
-    agregarContacto(contacto){
-
-        contacto.id = Agenda.
-
-
-    }
 
 
 
