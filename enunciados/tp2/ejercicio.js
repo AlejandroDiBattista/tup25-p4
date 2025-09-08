edb09bbf9d5bcb501f03ea422e5d9a71345f880f
Jorge Villagra <jorgeGamerx12@gmail.com>
2025-09-08 02:01:57 -0300

    yacimientos del tp2

--- enunciados/tp2/ejercicio.js
+++ enunciados/tp2/ejercicio.js
@@ -1,2 +1,51 @@
-'use strict';
-// Funciones generales
+class Contacto {
+    constructor(id,nombre,apellido, telefono, email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.id = id;
+        this.apellido = apellido;
+    }
+}
+class Agenda{
+constructor(){
+    this.Contactos = [];
+}
+//implementacion de metodos de clase 
+
+ agregar(contacto) {
+        this.contactos.push(contacto);
+    }
+
+    buscar(id) {
+        return this.contactos.find(c => c.id === id);
+    }
+
+    editar(id, nuevosDatos) {
+        const contacto = this.buscar(id);
+        if (contacto) {
+            Object.assign(contacto, nuevosDatos);
+            return true;
+        }
+        return false;
+    }
+
+    borrar(id) {
+        const index = this.contactos.findIndex(c => c.id === id);
+        if (index !== -1) {
+            this.contactos.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+
+    listar() {
+        return this.contactos;
+    }
+//agregar
+//buscar
+//editar
+//borrar
+//listar
+}
+module.exports = [Contacto,Agenda];
