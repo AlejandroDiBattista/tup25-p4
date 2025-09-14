2fde54b33efcd761a28d797682d82a9b95813cbf
Oscar40997 <Ignaciososa1ro@gmail.com>
2025-09-14 19:57:09 -0300

    cambiando mas

--- tp/61136 - Sosa, Oscar Ignacio/tp3/src/utils/text.js
+++ tp/61136 - Sosa, Oscar Ignacio/tp3/src/utils/text.js
@@ -8,3 +8,12 @@ export function norm(text) {
 export function cmpNombre(a, b) {
   return norm(a.nombre).localeCompare(norm(b.nombre));
 }
+
+export function includesContacto(contact, searchText) {
+  const normalizedSearch = norm(searchText);
+  return (
+    norm(contact.nombre).includes(normalizedSearch) ||
+    norm(contact.telefono).includes(normalizedSearch) ||
+    norm(contact.legajo).includes(normalizedSearch)
+  );
+}
