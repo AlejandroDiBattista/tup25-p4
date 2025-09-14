dbec1a3fc1b4e36b52830f3cf7f767a11f94484f
Oscar40997 <Ignaciososa1ro@gmail.com>
2025-09-14 20:10:45 -0300

    corrigiendo errores

--- tp/61136 - Sosa, Oscar Ignacio/tp3/src/App.jsx
+++ tp/61136 - Sosa, Oscar Ignacio/tp3/src/App.jsx
@@ -5,7 +5,29 @@ import Topbar from './components/Topbar';
 import ContactSection from './components/ContactSection';
 
 function App() {
-  return <h1>Hola</h1>
+  const [contacts, setContacts] = useState(loadAlumnos());
+  const [searchText, setSearchText] = useState('');
+
+  const filteredContacts = contacts.filter(contact =>
+    includesContacto(contact, searchText)
+  );
+
+  const favoritos = filteredContacts.filter(c => c.favorito);
+  const noFavoritos = filteredContacts.filter(c => !c.favorito);
+
+  const toggleFavorite = id => {
+    setContacts(contacts.map(contact =>
+      contact.id === id ? { ...contact, favorito: !contact.favorito } : contact
+    ));
+  };
+
+  return (
+    <>
+      <Topbar searchText={searchText} setSearchText={setSearchText} />
+      <ContactSection title="Favoritos" contacts={favoritos} toggleFavorite={toggleFavorite} />
+      <ContactSection title="Contactos" contacts={noFavoritos} toggleFavorite={toggleFavorite} />
+    </>
+  );
 }
 
-export default App
+export default App;
