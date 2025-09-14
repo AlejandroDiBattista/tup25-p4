4cbc19a0776ded6d8101f8d5923a9ae3b7b57b99
Oscar40997 <Ignaciososa1ro@gmail.com>
2025-09-14 20:01:15 -0300

    aumentando ajustes

--- tp/61136 - Sosa, Oscar Ignacio/tp3/src/App.jsx
+++ tp/61136 - Sosa, Oscar Ignacio/tp3/src/App.jsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+import { loadAlumnos } from './services/alumnos';
+import { includesContacto } from './utils/text';
+import Topbar from './components/Topbar';
+import ContactSection from './components/ContactSection';
 
 function App() {
   return <h1>Hola</h1>
