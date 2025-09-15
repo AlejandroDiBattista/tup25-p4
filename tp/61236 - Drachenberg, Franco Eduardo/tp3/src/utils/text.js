99e770f8f8da7dc7095779123c0444d0970ddd8a
Franco Drachenberg <francodrach@gmail.com>
2025-09-15 19:47:07 -0300

    Formato y componentes vacios

---
+++ tp/61236 - Drachenberg, Franco Eduardo/tp3/src/utils/text.js
@@ -0,0 +1,18 @@
+export function normalizar(text) {
+  return text.normalize("NFD").toLowerCase();
+}
+
+export function cmpNombre(a, b) {
+  return normalizar(a.nombre).localeCompare(normalizar(b.nombre));
+}
+
+export function includesContacto(contacto, busqueda) {
+  const busquedaNorm = normalizar(busqueda);
+  const { nombre, legajo, telefono } = contacto;
+
+  return (
+    normalizar(nombre).includes(busquedaNorm) ||
+    normalizar(legajo).includes(busquedaNorm) ||
+    normalizar(telefono).includes(busquedaNorm)
+  );
+}
