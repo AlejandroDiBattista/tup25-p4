5abfceecc9c5fb6cd086955285655080d75f3c8d
Santiago Martinez Campero <thesantiblocks@gmail.com>
2025-10-19 11:47:11 -0300

    Commit 1: Configuración inicial de la app Streamlit - página y sidebar básica

--- tp/61445 - Martínez Campero, Santiago/tp5/ejercicio.py
+++ tp/61445 - Martínez Campero, Santiago/tp5/ejercicio.py
@@ -4,4 +4,9 @@ import streamlit as st
 
 st.set_page_config(page_title="Reporte de productos", layout="wide")
 
+# Sidebar
+st.sidebar.title("Configuración")
+uploaded_file = st.sidebar.file_uploader("Seleccioná un CSV", type="csv")
+selected_year = st.sidebar.selectbox("Seleccioná un año", [])
+
 st.write("Escribir aca la solucion del TP5")
