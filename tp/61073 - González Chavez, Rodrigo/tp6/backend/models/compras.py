d4cb238f401186867c04d6e5992c205e6171fcb3
Rodrigo GC <gonzalezrodrigo118@gmail.com>
2025-11-03 20:09:14 -0300

    Solución de relaciones entre tablas de la BD

--- tp/61073 - González Chavez, Rodrigo/tp6/backend/models/compras.py
+++ tp/61073 - González Chavez, Rodrigo/tp6/backend/models/compras.py
@@ -11,6 +11,9 @@ class Compra(SQLModel, table=True):
     total: float = Field(default=0.0, ge=0)
     envio: float = Field(default=0.0, ge=0)
 
+    usuarios: Optional["Usuario"] = Relationship(back_populates="compras")
+    items: List[ItemCompra] =  Relationship()
+
 class ItemCompra(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     compra_id: int = Field(foreign_key="compra.id", index=True)
@@ -18,6 +21,3 @@ class ItemCompra(SQLModel, table=True):
     nombre: str = Field(default="", max_length=255)
     cantidad: int = Field(default=1, ge=1)
     precio_unitario: float = Field(default=0.0, ge=0)
-
-    usuarios: Optional["Usuario"] = Relationship(back_populates="compras")
-    items: List[ItemCompra] =  Relationship()
