9124a202e046a154324eff86432af49c34857b9e
Micaela <luzmicaelaponce00@gmail.com>
2025-11-10 00:32:49 -0300

    en frontend actualizo el header según el estado de autenticación

--- tp/61046 - Ponce, Luz Micaela/tp6/frontend/app/components/Header.tsx
+++ tp/61046 - Ponce, Luz Micaela/tp6/frontend/app/components/Header.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/link';
+import { cookies } from 'next/headers';
+import LogoutButton from './LogoutButton';
 
-export default function Header() {
+export default async function Header() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('token')?.value;
+
+
+  const estaLogueado = !!token;
+
   return (
     <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
+
           <div className="flex">
             <Link href="/" className="text-xl font-bold text-gray-900">
               TP6 Shop
@@ -18,18 +27,36 @@ export default function Header() {
             >
               Productos
             </Link>
-            <Link
-              href="/iniciar-sesion"
-              className="text-sm font-medium text-gray-700 hover:text-pink-500 transition-colors"
-            >
-              Ingresar
-            </Link>
-            <Link
-              href="/registrar"
-              className="text-sm font-bold text-gray-900 hover:text-pink-500 transition-colors"
-            >
-              Crear cuenta
-            </Link>
+
+            {estaLogueado ? (
+              <>
+                <Link
+                  href="/compras"
+                  className="text-sm font-medium text-gray-700 hover:text-pink-500 transition-colors"
+                >
+                  Mis compras
+                </Link>
+                <span className="text-sm font-bold text-pink-600">
+                  Mi Cuenta
+                </span>
+                <LogoutButton />
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/iniciar-sesion"
+                  className="text-sm font-medium text-gray-700 hover:text-pink-500 transition-colors"
+                >
+                  Ingresar
+                </Link>
+                <Link
+                  href="/registrar"
+                  className="text-sm font-bold text-gray-900 hover:text-pink-500 transition-colors"
+                >
+                  Crear cuenta
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
