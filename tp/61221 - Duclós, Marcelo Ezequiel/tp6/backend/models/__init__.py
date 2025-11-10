5a56b66648d1d289f3d7bc7236334343c1a2dcc1
EzeDuclos <ezequielduclos@gmail.com>
2025-11-09 21:20:26 -0300

    TP6 - 61221 - Duclós Marcelo Ezequiel
    Se crearon modelos básicos Usuario y Producto

--- tp/61221 - Duclós, Marcelo Ezequiel/tp6/backend/models/__init__.py
+++ tp/61221 - Duclós, Marcelo Ezequiel/tp6/backend/models/__init__.py
@@ -1,1 +1,13 @@
-# Este archivo permite que Python trate a 'models' como un paquete
+"""Modelos de la base de datos para la aplicación de comercio electrónico"""
+
+from .productos import Producto, ProductoResponse
+from .usuarios import Usuario, UsuarioCreate, UsuarioLogin, UsuarioResponse
+
+__all__ = [
+    "Producto", 
+    "ProductoResponse",
+    "Usuario", 
+    "UsuarioCreate", 
+    "UsuarioLogin", 
+    "UsuarioResponse"
+]
