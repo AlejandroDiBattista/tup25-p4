dc4bf80ca3da99dec6ac9ec78bfec3e04aae1ef8
JuanMo17 <juandanielmoralesxd@gmail.com>
2025-11-11 01:58:34 -0300

    feat(frontend): pantalla de inicio de sesión
    feat(frontend): pantalla de inicio de sesión

---
+++ tp/61338 - Morales, Juan Daniel/tp6/frontend/app/login/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useState } from "react";
+import { Auth } from "../services/productos";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState(""); const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false); const router = useRouter();
+
+  const submit = async (e: any) => {
+    e.preventDefault(); setLoading(true);
+    try { await Auth.login(email, password); router.push("/"); }
+    catch (e: any) { alert(e.message || "Error"); }
+    finally { setLoading(false); }
+  };
+
+  return (
+    <form onSubmit={submit} className="max-w-md mx-auto card space-y-3">
+      <h1 className="text-xl font-semibold">Iniciar sesión</h1>
+      <input className="input" placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} />
+      <input className="input" type="password" placeholder="Contraseña" value={password} onChange={e=>setPassword(e.target.value)} />
+      <button className="btn" disabled={loading}>{loading ? "Ingresando..." : "Ingresar"}</button>
+    </form>
+  );
+}
