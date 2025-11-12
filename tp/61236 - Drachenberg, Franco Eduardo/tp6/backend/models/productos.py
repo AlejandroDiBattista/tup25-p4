6b0e8888a9b5d8c65b69f83ea2f3539bda2cf4a5
Franco Drachenberg <francodrach@gmail.com>
2025-11-12 15:52:38 -0300

    Definir modelos SQLModel

--- tp/61236 - Drachenberg, Franco Eduardo/tp6/backend/models/productos.py
+++ tp/61236 - Drachenberg, Franco Eduardo/tp6/backend/models/productos.py
@@ -1,15 +1,15 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 
 
 class Producto(SQLModel, table=True):
-    """Modelo de Producto para la base de datos.
-    
-    TODO: Implementar los campos necesarios según las especificaciones.
-    """
+    #Modelo de Producto.
     id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str = Field(default="", max_length=255)
     descripcion: str = Field(default="")
     precio: float = Field(default=0.0, ge=0)
     categoria: str = Field(default="", max_length=100)
-    existencia: int = Field(default=0, ge=0) 
+    existencia: int = Field(default=0, ge=0)
+
+    items_carrito: list["ItemCarrito"] = Relationship(back_populates="producto")
+    items_compra: list["ItemCompra"] = Relationship(back_populates="producto")
